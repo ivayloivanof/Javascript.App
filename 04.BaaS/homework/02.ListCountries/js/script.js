1c83ca07206618b98b1d1ffb018be3ca38931575
ivanof <ivaylo.ivanof@gmail.com>
2016-03-05 21:03:09 +0200

    create add and delete method for kinvey.com

---
+++ 04.BaaS/homework/02.ListCountries/js/script.js
@@ -0,0 +1,190 @@
+var username = 'test';
+var password = 'test';
+var appKey = 'kid_byv0uGiy1-/';
+var urlKinvey = 'http://baas.kinvey.com/appdata/';
+
+//get all Country
+$.ajax({
+	url: urlKinvey + appKey + 'Country',
+	type: 'GET',
+	beforeSend: function (xhr) {
+		xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+	},
+	success: function (data) {
+		$("#list").append("<li>Country</li>");
+		data.forEach(function (element) {
+			//console.log(element);
+			$("#list").append("<li>Name: " + element.name + "; Continent: " + element.continent + "</li>");
+		});
+	}
+});
+
+//get all towns
+$.ajax({
+	url: urlKinvey + appKey + 'Town',
+	type: 'GET',
+	beforeSend: function (xhr) {
+		xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+	},
+	success: function (data) {
+		$("#list").append("<li>Towns</li>");
+		data.forEach(function (element) {
+			//console.log(element);
+			$("#list").append("<li>Name: " + element.name + "; Country: " + element.country + "</li>");
+		});
+	}
+});
+
+//add data
+$("#insert").on('click', function (event) {
+	if ($("#country").val() !== '' && $("#country").val() !== 'Country') {
+		var objCountry = {
+			"name": $("#country").val(),
+			"continent": "Empty"
+		};
+
+		$.ajax({
+			url: urlKinvey + appKey + 'Country',
+			type: 'POST',
+			data: objCountry,
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+			},
+			success: function (data) {
+				console.log(data);
+			}
+		});
+	}
+
+	if ($("#town").val() !== '' && $("#town").val() !== 'Town') {
+		var objTown = {
+			"name": $("#town").val(),
+			"country": "Other"
+		};
+
+		$.ajax({
+			url: urlKinvey + appKey + 'Town',
+			type: 'POST',
+			data: objTown,
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+			},
+			success: function (data) {
+				console.log(data);
+			}
+		});
+	}
+});
+
+//delete data
+$("#delete").on('click', function (event) {
+	if ($("#deleteCountry").val() !== '' && $("#deleteCountry").val() !== 'Country') {
+		$.ajax({
+			url: urlKinvey + appKey + 'Country/?query={"name":"'+$("#deleteCountry").val()+'"}',
+			type: 'DELETE',
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+			},
+			success: function (data) {
+				console.log(data);
+			}
+		});
+	}
+
+	if ($("#deleteTown").val() !== '' && $("#deleteTown").val() !== 'Town') {
+		$.ajax({
+			url: urlKinvey + appKey + 'Town/?query={"name":"'+$("#deleteTown").val()+'"}',
+			type: 'DELETE',
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+			},
+			success: function (data) {
+				console.log(data);
+			}
+		});
+	}
+
+});
+
+//update data
+$("#update").on('click', function (event) {
+	//if ($("#country").val() !== '' && $("#country").val() !== 'Country') {
+	//	var objCountry = {
+	//		"name": $("#country").val(),
+	//		"continent": "Empty"
+	//	};
+	//
+	//	$.ajax({
+	//		url: urlKinvey + appKey + 'Country',
+	//		type: 'POST',
+	//		data: objCountry,
+	//		beforeSend: function (xhr) {
+	//			xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+	//		},
+	//		success: function (data) {
+	//			console.log(data);
+	//		}
+	//	});
+	//}
+
+	if ($("#updateTown").val() !== '' && $("#updateTown").val() !== 'Town') {
+		//?query={"firstName":"James"}
+
+		$.ajax({
+			url: urlKinvey + appKey + 'Country/?query={"name":"'+$("#updateTown").val()+'"}',
+			type: 'PUT',
+			data: objTown,
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+			},
+			success: function (data) {
+				console.log(data);
+			}
+		});
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#country").on('click', function (event) {
+	$("#country").val('');
+});
+
+$("#town").on('click', function (event) {
+	$("#town").val('');
+});
